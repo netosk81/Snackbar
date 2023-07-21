b475d4681846346b12bb0c0012c6fe5d328e40b7
jose ribeiro neto <hk.neto@gmail.com>
2023-07-20 21:05:46 -0300

    Adicionando funcionalidade de click no botao do Snackbar

--- app/src/main/java/com/example/snackbar/MainActivity.java
+++ app/src/main/java/com/example/snackbar/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.buttonAbrir);
+
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(
+                        view,
+                        "Botão clicado",
+                        Snackbar.LENGTH_LONG
+                ).show();
+            }
+        });
+    }
+    public void abrirSnackbar(View view){
+
     }
 }
